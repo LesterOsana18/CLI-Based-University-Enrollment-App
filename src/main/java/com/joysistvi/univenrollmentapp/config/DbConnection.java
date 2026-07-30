cdcf09e9203a447660ea20c9fad4c94f5471f61e
Lester <lstrosana.18@gmail.com>
2026-07-31 00:55:42 +0800

    Implement database connection class with JDBC standard practices

--- src/main/java/com/joysistvi/univenrollmentapp/config/DbConnection.java
+++ src/main/java/com/joysistvi/univenrollmentapp/config/DbConnection.java
@@ -1,5 +1,19 @@
 package com.joysistvi.univenrollmentapp.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// JDBC Standard Practice
 public class DbConnection {
 
+    // Database connection parameters
+    private static final String URL = "";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+
+    // Create and return a database connection
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
