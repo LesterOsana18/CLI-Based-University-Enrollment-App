7c2d0c8a61ca034718535ec0751de104dea2ad7f
Lester <lstrosana.18@gmail.com>
2026-07-31 02:00:57 +0800

    Implement UserService and UserServiceImpl for user management operations

--- src/main/java/com/joysistvi/univenrollmentapp/service/UserService.java
+++ src/main/java/com/joysistvi/univenrollmentapp/service/UserService.java
@@ -1,5 +1,28 @@
 package com.joysistvi.univenrollmentapp.service;
 
+import java.util.List;
+
+import com.joysistvi.univenrollmentapp.model.User;
+
+// Service Interface
+// Defines the business operations for User objects
 public interface UserService {
-    
+
+    // Retrieve all users
+    List<User> getAllUsers();
+
+    // Retrieve a user by ID
+    User getUserById(int id);
+
+    // Authenticate a user
+    User login(String username, String password);
+
+    // Register a new user
+    boolean register(User user);
+
+    // Update an existing user
+    boolean updateUser(User user);
+
+    // Delete a user
+    boolean deleteUser(int id);
 }
--- src/main/java/com/joysistvi/univenrollmentapp/service/UserServiceImpl.java
+++ src/main/java/com/joysistvi/univenrollmentapp/service/UserServiceImpl.java
@@ -1,5 +1,89 @@
 package com.joysistvi.univenrollmentapp.service;
 
-public class UserServiceImpl {
-    
+import java.util.List;
+
+import com.joysistvi.univenrollmentapp.model.User;
+import com.joysistvi.univenrollmentapp.repository.UserRepository;
+import com.joysistvi.univenrollmentapp.utils.PasswordUtils;
+
+// Service Implementation
+// Implements the business operations for User objects
+public class UserServiceImpl implements UserService {
+
+    // Dependency Injection
+    private final UserRepository userRepository;
+
+    // Constructor
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // Retrieve all users
+    @Override
+    public List<User> getAllUsers() {
+
+        // TODO
+        return null;
+
+    }
+
+    // Retrieve a user by ID
+    @Override
+    public User getUserById(int id) {
+
+        // TODO
+        return null;
+
+    }
+
+    // Authenticate a user
+    @Override
+    public User login(String username, String password) {
+
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            return null;
+        }
+
+        if (!PasswordUtils.verifyPassword(password, user.getPassword())) {
+            return null;
+        }
+
+        return user;
+
+    }
+
+    // Register a new user
+    @Override
+    public boolean register(User user) {
+
+        if (userRepository.usernameExists(user.getUsername())) {
+            return false;
+        }
+
+        user.setPassword(
+                PasswordUtils.hashPassword(user.getPassword()));
+
+        return userRepository.save(user);
+
+    }
+
+    // Update an existing user
+    @Override
+    public boolean updateUser(User user) {
+
+        // TODO
+        return false;
+
+    }
+
+    // Delete a user
+    @Override
+    public boolean deleteUser(int id) {
+
+        // TODO
+        return false;
+
+    }
 }
