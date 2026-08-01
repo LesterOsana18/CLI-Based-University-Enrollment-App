3c8392641d78652562295409ac60ce348bc1fa80
Lester <lstrosana.18@gmail.com>
2026-08-02 04:21:17 +0800

    Add comments to DbConnection class for clarity on configuration and JDBC practices

--- src/main/java/com/joysistvi/univenrollmentapp/config/DbConnection.java
+++ src/main/java/com/joysistvi/univenrollmentapp/config/DbConnection.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// Config Class
+// JDBC Standard Practice
 public class DbConnection {
 
     // Database connection parameters
