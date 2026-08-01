b8ec2457519ba5027ccc759030df3ec8c1a49475
Lester <lstrosana.18@gmail.com>
2026-08-01 23:26:40 +0800

    Remove hardcoded database password from DbConnection class for security

--- src/main/java/com/joysistvi/univenrollmentapp/config/DbConnection.java
+++ src/main/java/com/joysistvi/univenrollmentapp/config/DbConnection.java
@@ -9,8 +9,9 @@ public class DbConnection {
     // Database connection parameters
     private static final String URL = "jdbc:mysql://localhost:3306/university_enrollment_db";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "root@123";
+    private static final String PASSWORD = ""; // Insert your database password here (if applicable)
 
+    // Method to establish and return a database connection
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
