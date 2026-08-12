e290f732cecbfa306ffe3123cace91c203e3dd9c
Lester <lstrosana.18@gmail.com>
2026-08-13 02:09:02 +0800

    Clear database connection parameters in DbConnection class for security reasons

--- src/main/java/com/joysistvi/univenrollmentapp/config/DbConnection.java
+++ src/main/java/com/joysistvi/univenrollmentapp/config/DbConnection.java
@@ -9,9 +9,9 @@ import java.sql.SQLException;
 public class DbConnection {
 
     // Database connection parameters
-    private static final String URL = "jdbc:mysql://localhost:3306/university_enrollment_db";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "lesterosana"; // Insert your database password here (if applicable)
+    private static final String URL = ""; // Insert your database URL here (e.g., jdbc:mysql://localhost:3306/your_database_name)
+    private static final String USERNAME = ""; // Insert your database username here
+    private static final String PASSWORD = ""; // Insert your database password here (if applicable)
 
     // Method to establish and return a database connection
     public Connection getConnection() throws SQLException {
